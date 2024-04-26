69b3f7702a0cd9a899601481d2d07bf8142035cd
H.Huseyin Sahin <huseyin.sahin.3321@gmail.com>
2024-04-26 15:28:41 +0300

    feat: authentication service

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -3,7 +3,9 @@ import express from "express";
 const router = express.Router();
 
 router.post("/api/users/signout", (req, res) => {
-  
+  req.session = null;
+
+  res.status(204).send({});
 });
 
 export { router as signoutRouter };
