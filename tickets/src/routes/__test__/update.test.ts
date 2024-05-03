7f56bf852f89dd1611273d617cc6ea2fa214409a
H.Huseyin Sahin <huseyin.sahin.3321@gmail.com>
2024-05-03 16:48:49 +0300

    feat: add nats streaming service

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -1,7 +1,7 @@
 import request from "supertest";
 import { app } from "../../app";
-import { Ticket } from "../../models/ticket";
 import mongoose from "mongoose";
+import { natsWrapper } from "../../nats-wrapper";
 
 it("returns a 404 if the provided id does not exist", async () => {
   const id = new mongoose.Types.ObjectId().toHexString();
@@ -97,4 +97,29 @@ it("updates the ticket provided valid inputs", async () => {
 
   expect(res.body.title).toEqual(title);
   expect(res.body.price).toEqual(price);
+});
+
+it("publishes an event", async () => {
+  const cookie = global.signin();
+
+  const newTicketRes = await request(app)
+    .post("/api/tickets")
+    .set("Cookie", cookie)
+    .send({
+      title: "title1",
+      price: 20,
+    });
+
+  const title = "updated_title1";
+  const price = 10;
+
+  await request(app)
+    .put(`/api/tickets/${newTicketRes.body.id}`)
+    .set("Cookie", cookie)
+    .send({
+      title,
+      price,
+    }).expect(200);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
 });
